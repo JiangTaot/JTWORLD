f3e378008e10a9051f0a78a453ccdb219a918882
jiangtao <2547236893@qq.com>
2024-04-28 16:53:22 +0800

    路由

--- JT-LEARNING/learning_vue/src/router/routes.js
+++ JT-LEARNING/learning_vue/src/router/routes.js
@@ -1,12 +1,15 @@
-import HelloWorld from "../components/HelloWorld.vue";
-import App from "../App.vue";
 import Login from "../page/Login.vue";
 import Index from "../page/Index.vue";
+import HelloWorld from "../components/HelloWorld.vue";
 
 const routes = [
     {
-        path: '/hello',
+        path: '/',
         component: Index
+    },
+    {
+        path: '/hello',
+        component: HelloWorld
     },
     {
         path: '/login',
